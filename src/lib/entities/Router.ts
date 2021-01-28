397e47f05fa37f224eb7d4b2f1b7b013197e1754
aslanorlov <aslanorlov@gmail.com>
2021-01-28 19:19:12 +0200

    Added window.location.search for createRouteFromLocationWithReplace

--- src/lib/entities/Router.ts
+++ src/lib/entities/Router.ts
@@ -494,7 +494,9 @@ export class Router extends EventEmitter<{
     }
 
     private createRouteFromLocationWithReplace() {
-        const location = this.useHash ? window.location.hash : window.location.pathname;
+        const location = this.useHash
+            ? window.location.hash
+            : window.location.pathname + window.location.search;
 
         try {
             return MyRoute.fromLocation(this.routes, location, this.alwaysStartWithSlash);
